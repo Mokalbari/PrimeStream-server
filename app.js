a2fd0e88386c613a7f62fc209fa13b67b51af96b
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-07-27 10:31:30 +0200

    init + add routes in app to fetch data

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 import express from "express"
+import primeStreamData from "./primeStreamData.js"
+import cors from "cors"
+
+// App configuration
 const app = express()
-
-const sayHello = (req, res) => res.send("Hello World!")
-app.get("/", sayHello)
-
+app.use(cors())
 const port = 4903
+
+// Main route to get all the data
+app.get("/", (req, res) => res.json(primeStreamData))
+
+// Route to fetch movies
+app.get("/movies", (req, res) => {
+  const movies = primeStreamData.filter(data => data.category === "Movie")
+  res.json(movies)
+})
+
+// Route to fetch TV Series
+app.get("/tvseries", (req, res) => {
+  const tvSeries = primeStreamData.filter(data => data.category === "TV Series")
+  res.json(tvSeries)
+})
+
+// Route to fetch by name
+app.get("/:name", (req, res) => {
+  const name = req.params.name.toLowerCase()
+  const movieList = primeStreamData.filter(movie =>
+    movie.title.toLowerCase().includes(name),
+  )
+  res.json(movieList)
+})
+
 app.listen(port, () =>
   console.log(`The app is running on http://localhost:${port}`),
 )
